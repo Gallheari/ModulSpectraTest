0a6bc0f64e00279bdf686d9d1b69e520e3897bb7
gallheari <gallheari@gmail.com>
2026-02-14 07:18:51 +0000

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBK3JwG8PEEhyeM8vTRHuO-CWsV0OaVa6w",
+  authDomain: "modulspectratest.firebaseapp.com",
+  projectId: "modulspectratest",
+  storageBucket: "modulspectratest.appspot.com",
+  messagingSenderId: "1088136697187",
+  appId: "1:1088136697187:web:2236ff295eb98a5ef3ef56",
+  measurementId: "G-W8KHVP7HQM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- src/pages/Dwaid/index.jsx
+++ src/pages/Dwaid/index.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
 const Dwaid = () => {
-  return <h1>Moduł Dawid</h1>;
+  const containerStyle = {
+    padding: '2rem',
+    textAlign: 'center',
+  };
+
+  const headerStyle = {
+    fontSize: '3rem',
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    marginBottom: '2rem',
+  };
+
+  const textStyle = {
+    fontSize: '1.5rem',
+    color: '#333',
+    lineHeight: '1.6',
+  };
+
+  return (
+    <div style={containerStyle}>
+      <h1 style={headerStyle}>Witaj w module Dawid!</h1>
+      <p style={textStyle}>
+        To jest dynamicznie renderowany, kolorowy tekst, który ma za zadanie
+        przyciągnąć Twoją uwagę i pokazać możliwości Reacta!
+      </p>
+    </div>
+  );
 };
 
 export default Dwaid;
